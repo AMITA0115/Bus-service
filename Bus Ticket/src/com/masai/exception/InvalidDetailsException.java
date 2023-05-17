ae703de91ccfda3c0a20272996112e42705d8cae
Amita <119422241+AMITA0115@users.noreply.github.com>
2023-05-17 17:08:44 +0530

    Add files via upload
    updated

---
+++ Bus Ticket/src/com/masai/exception/InvalidDetailsException.java
@@ -0,0 +1,12 @@
+package com.masai.exception;
+
+public class InvalidDetailsException extends Exception {
+	public InvalidDetailsException() {
+
+	}
+
+	public InvalidDetailsException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ Bus Ticket/src/com/masai/utility/Admin.java
@@ -0,0 +1,7 @@
+package com.masai.utility;
+
+public interface Admin {
+	public static String username = "admin";
+	public static String password = "password";
+
+}
